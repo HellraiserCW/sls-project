f772c37467bc777741732b8399b0e94fffa87956
Kostiantyn Sakharov <hellraisercw@gmail.com>
2024-09-17 10:18:46 +0300

    feat: add get by secondary index

---
+++ src/controllers/get-by-email.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { dynamodb } from '../dynamodb';
+
+export const getUserByEmail = (req: Request, res: Response) => {
+  const params = {
+    TableName: process.env.tableName!,
+    IndexName: 'EmailIndex',
+    KeyConditionExpression: 'email = :email',
+    ExpressionAttributeValues: {
+      ':email': req.params.email,
+    },
+  };
+
+  dynamodb.query(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({
+        error: 'Failed to retrieve user by email from db',
+      });
+
+      return;
+    }
+    console.log(result);
+    res.status(200).json(result.Items);
+  });
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { listUsers } from '../controllers/list';
 import { getUser } from '../controllers/get';
+import { getUserByEmail } from '../controllers/get-by-email';
 import { createUser } from '../controllers/create';
 import { updateUser } from '../controllers/update';
 import { deleteUser } from '../controllers/delete';
@@ -9,6 +10,7 @@ const router = Router();
 
 router.get('/', listUsers);
 router.get('/:id', getUser);
+router.get('/by-email/:email', getUserByEmail);
 router.post('/', createUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
