674eed54815463ebb88679f8345b9bad75ef3e21
Kostiantyn Sakharov <hellraisercw@gmail.com>
2024-09-25 11:54:09 +0300

    fix: migrations

--- offline/migrations/users.ts
+++ offline/migrations/users.ts
@@ -1,8 +1,8 @@
 import { writeFileSync } from "fs";
 import { join } from "path";
-import dotenv from "dotenv";
+import { config } from "dotenv";
 
-dotenv.config();
+config();
 
 interface AttributeDefinition {
   AttributeName: string;
